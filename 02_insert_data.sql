8ea4aba8e819ee36d2446de0cd71c332f402c71e
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-11-25 22:56:38 -0600

    Create 02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,23 @@
+INSERT INTO productos (nombre, precio, stock) VALUES
+('Laptop', 999.99, 10),
+('Pantalla', 300.50, 5),
+('Memoria', 85.00, 7),
+('Ratón', 99.90, 8),
+('Ventilador', 348.00, 9),
+('Silla', 215.75, 4),
+('SSD', 59.99, 3),
+('Celular', 599.00, 2),
+('Cargador', 39.99, 1),
+('Cable', 45.20, 6);
+
+INSERT INTO departamentos (nombre_depto) VALUES
+('Ventas'),
+('Ingeniería'),
+('Desarrollo');
+
+INSERT INTO empleados (nombre, email, id_depto) VALUES
+('Angel Hernandez', 'angel@ejemplo.com', 1),
+('Aurelio Medina', 'au@ejemplo.com', 2),
+('Monse Rangel', 'mon@ejemplo.com', 1),
+('Miguel Arellano', 'arellano@ejemplo.com', 2),
+('Fatima Cuevas', 'Cuevas@ejemplo.com', 3);
